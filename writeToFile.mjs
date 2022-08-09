7cefb0d4031afce7b88cea0a81af86d3e1026c9c
antheboets <anthe.boets@student.ehb.be>
2022-08-09 23:35:33 +0200

    making write and read lib

--- writeToFile.mjs
+++ writeToFile.mjs
@@ -1,7 +1,9 @@
 import * as fs from 'node:fs/promises'
 
-export function writeFile(filename,data){
-    fs.writeFile(filename + "1.json",data, function () {})
+export async function writeJson(filename,data){
+    await fs.writeFile(`${filename}.json`,JSON.stringify(data,null,3), function () {})
 }
 
-export default writeFile
+export async function readJson(path){
+    return JSON.parse(await fs.readFile(`./${path}.json`, 'utf8'))
+}
